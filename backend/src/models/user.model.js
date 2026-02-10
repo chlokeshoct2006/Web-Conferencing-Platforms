5d892bfd4606bd9f8624ecfcd8a7ec4aa0402395
Lokesh Chaudhary <chlokeshoct2006@gmail.com>
2026-02-10 23:06:45 +0530

    changes added

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,9 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 const userScheme = new Schema(
     {
-        name: {type; String, required:true},
-        username: {type; String, required:true, unique:true},
-        password: {type; String, required:true},
+        name: {type: String, required:true},
+        username: {type: String, required:true, unique:true},
+        password: {type: String, required:true},
         token: {type: String}
     }
 )
