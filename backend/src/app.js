63471e45b27a783fad6ac89a478ee28be3b6f025
Lokesh Chaudhary <chlokeshoct2006@gmail.com>
2026-02-10 23:06:01 +0530

    changes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,24 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import connectToSocket from "./controllers/socketManager.js";
 import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.get("/home", (req, res) => {
-  return res.json({ hello: "world" });
-});
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 const start = async () => {
 app.set("mongo_user")
   const connectionDB = await mongoose.connect(
     "mongodb+srv://chlokeshoct2006_db_user:lokesh_1234@cluster0.nfe3yhz.mongodb.net/",
   );
+  console.log(`Mongo Connected DB Host: ${connectionDB.connection.host}`)
   server.listen(app.get("port"), () => {
     console.log("LISTENING ON PORT 8000");
   });
