1663552bc1f2f1cdeec37c57d4f8a4ca81e52bdd
Lokesh Chaudhary <chlokeshoct2006@gmail.com>
2026-03-11 20:54:42 +0530

    make changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,9 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
-import connectToSocket from "./controllers/socketManager.js";
+import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -14,14 +15,16 @@ app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
+app.use("/api/v1/users", userRoutes);
+
 const start = async () => {
-app.set("mongo_user")
+  app.set("mongo_user")
   const connectionDB = await mongoose.connect(
-    "mongodb+srv://chlokeshoct2006_db_user:lokesh_1234@cluster0.nfe3yhz.mongodb.net/",
+    "mongodb+srv://chlokeshoct2006_db_user:lokesh_1234@cluster0.nfe3yhz.mongodb.net/?appName=Cluster0"
   );
   console.log(`Mongo Connected DB Host: ${connectionDB.connection.host}`)
   server.listen(app.get("port"), () => {
     console.log("LISTENING ON PORT 8000");
   });
-};
+}
 start();
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-const router = Router();
-router.route("/login")
-router.route("/register")
-router.route("/add_to_activity")
-router.route("/get_all_activity")
-export default router;
+import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
 
+
+
+const router = Router();
+
+router.route("/login").post(login)
+router.route("/register").post(register)
+router.route("/add_to_activity").post(addToHistory)
+router.route("/get_all_activity").get(getUserHistory)
+
+export default router;
